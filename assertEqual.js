240beb830f6f41ad77eb635220bc05527aec684e
joycuicui <cuiqijoy@outlook.com>
2023-11-13 16:44:04 -0800

    Add more test cases

--- assertEqual.js
+++ assertEqual.js
@@ -8,3 +8,7 @@ const assertEqual = function (actual, expected) {
 
 assertEqual("Lighthouse Labs", "Bootcamps");
 assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual("world", "wolrd");
+assertEqual(78, 78);
+assertEqual(89, 99);
