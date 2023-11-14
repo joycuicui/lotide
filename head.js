62f87f25ad464968abe53a2169fd5b649f44d78c
joycuicui <cuiqijoy@outlook.com>
2023-11-13 16:57:42 -0800

    Correct test case

--- head.js
+++ head.js
@@ -13,4 +13,4 @@ const head = function (arr) {
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([9]), 9);
-assertEqual(head([], undefined));
+assertEqual(head([]), undefined);
