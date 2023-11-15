a8b8387462008488f961aafcfc9e35c74ffe6b15
joycuicui <cuiqijoy@outlook.com>
2023-11-14 17:20:16 -0800

    create an assert arrays function

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("✅✅✅ Assertion passed: your arrays are equal.");
+  } else {
+    console.log("🛑🛑🛑 Assertion failed: your arrays are not the same.");
+  }
+};
