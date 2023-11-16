e01bcc9515bb4389125ee7ef37aa8e03c40f356b
joycuicui <cuiqijoy@outlook.com>
2023-11-15 16:09:21 -0800

    create a countLetters function

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (string) {
+  let result = {};
+  const noSpaceString = string.replaceAll(" ", "");
+  console.log(noSpaceString);
+
+  for (letter of noSpaceString) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
