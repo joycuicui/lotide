f4f95ebfe8e7c5156015b597b41259396cf941b9
joycuicui <cuiqijoy@outlook.com>
2023-11-16 15:58:56 -0800

    remove [] in assertArraysEqual function

--- middle.js
+++ middle.js
@@ -12,9 +12,9 @@ const eqArrays = function (arr1, arr2) {
 
 const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion passed: [${arr1} === ${arr2}]`);
+    console.log(`✅✅✅ Assertion passed: ${arr1} === ${arr2}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion failed: [${arr1}] !== [${arr2}]`);
+    console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`);
   }
 };
 
