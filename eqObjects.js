4bc3d67804a69d8311109a00ab9582107f5c64fe
joycuicui <cuiqijoy@outlook.com>
2023-11-15 19:21:43 -0800

    add return false if eqArray returns false

--- eqObjects.js
+++ eqObjects.js
@@ -26,8 +26,10 @@ const eqObjects = function (object1, object2) {
   }
   const key1 = Object.keys(object1);
   for (const key of key1) {
-    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
-      eqArrays(object1[key], object2[key]);
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
     } else if (object1[key] !== object2[key]) {
       return false;
     }
