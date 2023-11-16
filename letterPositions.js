6871b4b47c3c90a56a533e39dfdd1a46a6d26998
joycuicui <cuiqijoy@outlook.com>
2023-11-15 16:47:26 -0800

    create a letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion passed: [${arr1} === ${arr2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]);
