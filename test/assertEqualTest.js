438631f131fd18edecd71a6f9ca18654ead6ede2
joycuicui <cuiqijoy@outlook.com>
2023-11-20 09:57:32 -0800

    test files

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require("../assertEqual");
+
+assertEqual("Lighthouse Labs", "Bootcamps");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual("world", "wolrd");
+assertEqual(78, 78);
+assertEqual(89, 99);
