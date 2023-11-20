d11d9d01b2d25ec2c613f6da0aec2bf7c07b4d67
joycuicui <cuiqijoy@outlook.com>
2023-11-19 19:02:19 -0800

    create a flattern function

---
+++ flatten.js
@@ -0,0 +1,33 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const flatten = function (arr) {
+  const output = [];
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      for (const element of item) {
+        output.push(element);
+      }
+    } else {
+      output.push(item);
+    }
+  }
+  return output;
+};
