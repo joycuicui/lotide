72f5b547fa7b4dbfc24707fef764d1713f875746
joycuicui <cuiqijoy@outlook.com>
2023-11-20 12:51:47 -0800

    add more test cases

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,5 +2,17 @@ const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+assertEqual(tail(words).length, 2);
+assertEqual(tail(words)[0], "Lighthouse");
+
+const numbers = [1, 2, 3, 4, 5];
+assertEqual(tail(numbers).length, 4);
+assertEqual(tail(numbers)[0], 2);
+
+const test1 = [1];
+assertEqual(tail(test1).length, 0);
+assertEqual(tail(test1)[0], undefined);
+
+const test2 = [];
+assertEqual(tail(test2).length, 0);
+assertEqual(tail(test2)[0], undefined);
