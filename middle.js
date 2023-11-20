34edfb0d157c00b41c0d603b09b1b62e4142aace
joycuicui <cuiqijoy@outlook.com>
2023-11-20 13:48:49 -0800

    refactor middle using modules

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const middle = function (array) {
   let newArray = [];
   if (array.length <= 2) {
@@ -33,8 +13,4 @@ const middle = function (array) {
   return newArray;
 };
 
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([5]), []);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2]);
+module.exports = middle;
