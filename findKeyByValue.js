5f75acce30a8c49bd3d9e73921d9f61128069de5
joycuicui <cuiqijoy@outlook.com>
2023-11-20 18:53:14 -0800

    test findKeyByValue using mocha and chai

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,6 +1,6 @@
 const findKeyByValue = function (object, value) {
   const keys = Object.keys(object);
-  for (let key of keys) {
+  for (const key of keys) {
     if (object[key] === value) {
       return key;
     }
@@ -8,15 +8,3 @@ const findKeyByValue = function (object, value) {
 };
 
 module.exports = findKeyByValue;
-
-// const bestTVShowsByGenre = {
-//   sci_fi: "The Expanse",
-//   comedy: "Brooklyn Nine-Nine",
-//   drama: "The Wire",
-// };
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-
-// const dog = { name: "puppy", age: 5, breed: "poodle" };
-// assertEqual(findKeyByValue(dog, 5), "age");
-// assertEqual(findKeyByValue(dog, "poodle"), "breed");
