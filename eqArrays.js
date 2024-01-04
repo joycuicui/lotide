ae3e500a991e1ca60253248fdf9c03344d6e63aa
joycuicui <cuiqijoy@outlook.com>
2024-01-04 14:25:42 -0800

    modify eqArrays so it uses recursion to handle nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -3,7 +3,11 @@ const eqArrays = function (arr1, arr2) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
